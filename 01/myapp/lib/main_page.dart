c7e45e7f09416612ce25125ad30d7a1ce5746ff7
yeeun <charm5231@naver.com>
2020-08-13 15:43:01 +0900

    update folder tree

---
+++ 01/myapp/lib/main_page.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Movie')),
+      body: Center(child: Text('Hello Flutter')),
+    );
+  }
+}
