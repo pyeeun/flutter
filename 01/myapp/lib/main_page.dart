89aa3a09bd10f06f40eaa7451a57ef3a2bfe3031
yeeun <charm5231@naver.com>
2020-08-21 17:24:27 +0900

    2020.08.21 : add image

--- 01/myapp/lib/main_page.dart
+++ 01/myapp/lib/main_page.dart
@@ -5,33 +5,37 @@ class StatelessPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Taste'),
-        leading: Icon(Icons.favorite, color: Colors.red),
-      ),
+          title: Text('Taste'),
+          leading: Icon(Icons.favorite, color: Colors.red),
+
+          // 우측 팝업 버튼
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              icon: Icon(Icons.sort),
+              onSelected: (value) {
+                if (value == 'like')
+                  print('좋아요순');
+                else if (value == 'star')
+                  print('별점순');
+                else if (value == 'review')
+                  print('리뷰순');
+                else if (value == 'new') print('최신순');
+              },
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(value: 'like', child: Text('좋아요순')),
+                  PopupMenuItem(value: 'star', child: Text('별점순')),
+                  PopupMenuItem(value: 'review', child: Text('리뷰순')),
+                  PopupMenuItem(value: 'new', child: Text('최신순')),
+                ];
+              },
+            )
+          ]),
     );
   }
 }
 
 /*
-          actions: <Widget>[
-            PopupMenuButton<int>(
-              icon: Icon(Icons.sort),
-              onSelected: (value) {
-                if (value == 0)
-                  print("예매율순");
-                else if (value == 1)
-                  print("큐레이션");
-                else
-                  print("최신순");
-              },
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem(value: 0, child: Text("예매율순")),
-                  PopupMenuItem(value: 1, child: Text("큐레이션")),
-                  PopupMenuItem(value: 2, child: Text("최신순"))
-                ];
-              },
-            )
-          ]),
+
       body: Center(child: Text('Hello Flutter!')
       */
